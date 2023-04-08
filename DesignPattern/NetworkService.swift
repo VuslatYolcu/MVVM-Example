b5d4c0ec798c29ef8a3b54c50b2a6e4c450d1f7c
VuslatYolcu <ylcvalt@gmail.com>
2023-04-08 15:46:22 +0300

    Add a mock network service

---
+++ DesignPattern/NetworkService.swift
@@ -0,0 +1,28 @@
+//
+//  NetworkService.swift
+//  DesignPattern
+//
+//  Created by Vuslat Yolcu on 8.04.2023.
+//
+
+import Foundation
+
+final class NetworkService {
+    static let shared = NetworkService()
+    
+    var user: User?
+    
+    private init() { }
+    
+    func login(email: String, password: String, completion: @escaping(Bool) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            if email == "test@test.com" && password == "password" {
+                self?.user = User(firstName: "Vuslat", lastName: "Yolcu", email: "test@test.com", age: 26)
+                completion(true)
+            } else {
+                self?.user = nil
+                completion(false)
+            }
+        }
+    }
+}
