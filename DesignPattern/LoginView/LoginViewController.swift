d9883ba75a8ff736921cc44d88d981d87c0fc0e1
VuslatYolcu <ylcvalt@gmail.com>
2023-04-08 16:06:07 +0300

    Implement the binder

--- DesignPattern/LoginView/LoginViewController.swift
+++ DesignPattern/LoginView/LoginViewController.swift
@@ -17,11 +17,30 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        setupBinders()
+    }
+    
+    private func setupBinders() {
+        viewModel.error.bind { [weak self] error in
+            if let error = error {
+                print(error)
+            } else {
+                self?.goToHomePage()
+            }
+        }
+    }
+    
+    private func goToHomePage() {
+        let controller = storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        present(controller, animated: true, completion: nil)
     }
     
     @IBAction func loginBtnClicked(_ sender: UIButton) {
-        
+        guard let email = emailField.text, let password = passwordField.text else {
+            print("Please enter email and password")
+            return
+        }
+        viewModel.login(email: email, password: password)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
